cec374bafa1ade8efd7c2d955453c63a8c8cad4a
David Studholme <d.j.studholme@exeter.ac.uk>
2025-11-18 11:09:31 +0000

    created directory for downloading FASTQ files

---
+++ 01-sequence-reads/commands.sh
@@ -0,0 +1,6 @@
+### Obtain the fastq-dump tool from NBI
+wget https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/3.2.1/sratoolkit.3.2.1-ubuntu64.tar.gz
+tar zxvf sratoolkit.3.2.1-ubuntu64.tar.gz 
+ln -s sratoolkit.3.2.1-ubuntu64/bin/fastq-dump .
+
+### Download FASTQ files from SRA
