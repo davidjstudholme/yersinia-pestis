227f4d35e46856af32767d25636b45873c0a9ba1
David Studholme <d.j.studholme@exeter.ac.uk>
2025-11-18 10:52:00 +0000

    Added installation test

--- gatk/commands.sh
+++ gatk/commands.sh
@@ -1,8 +1,10 @@
-### Install GATK via Conda
-
+### Install GATK
 conda config --set auto_update_conda false
 conda config --set solver libmamba
 wget https://github.com/broadinstitute/gatk/releases/download/4.6.2.0/gatk-4.6.2.0.zip
 unzip gatk-4.6.2.0.zip
 conda env create -f gatk-4.6.2.0/gatkcondaenv.yml
 conda activate gatk
+
+### Check that GATK is installed
+./gatk-4.6.2.0/gatk --help
